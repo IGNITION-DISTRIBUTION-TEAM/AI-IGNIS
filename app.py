47b3eab5e8d2df3b45396c0d4cd4141d72b531e0
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-09-23 15:16:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def render_message(msg: Message):
 
 with st.sidebar:
     st.title(":blue[Snowflake Intellegence]")
-    st.button("Reset", type="primary")
+    st.button("New Chat", icon=":material/chat:", width="stretch")
 
 
 
