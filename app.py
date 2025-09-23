53aa042dcb4779682598dbcaa866bec600334c75
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-09-23 15:14:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -159,12 +159,8 @@ def render_message(msg: Message):
                     )
 
 with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
-
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
+    st.title("blue[Snowflake Intellegence]")
+    st.button("Reset", type="primary")
 
 
 
