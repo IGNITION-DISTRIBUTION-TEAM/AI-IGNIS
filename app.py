d0cbc1b70903ae2302b736240a9d5764569025f2
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-09-26 10:29:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,9 +167,7 @@ with st.sidebar:
     with st.popover("Recents", icon=":material/chat:", width="stretch"):
         genre = st.radio("Please select the recent chat",["hello", "show me all sales"],index=None,)
     st.title("")   
-    left = st.columns(1, border=True)
-
-    left.markdown("Lorem ipsum " * 10)
+    st.columns(1, border=True)
 
 st.title("Good afternoon, Andre")
 st.title(":blue[What insights can I help with?]")
