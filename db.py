44bc6b852fae985a5fb0fbcaec933ef7f40aa335
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-10-01 02:45:13 +0200

    Update db.py

--- db.py
+++ db.py
@@ -6,6 +6,24 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def get_snowflake_connection():
+    account = "pm58521.east-us-2.azure"   # Snowflake account
+    user = "STREAMLITAI"                   # Snowflake user
+
+    private_key_path = "keys/rsa_key.p8"
+    with open(private_key_path, "rb") as pem_in:
+        private_key = load_pem_private_key(pem_in.read(), password=None, backend=default_backend())
+
+    conn = snowflake.connector.connect(
+        user=user,
+        account=account,
+        private_key=private_key,
+        warehouse="CORTEX_WH",
+        database="SNOWFLAKE_INTELLIGENCE",
+        schema="STREAMLITAI"
+    )
+    return conn
+
 account = "pm58521".upper()
 user = "STREAMLITAI".upper()
 qualified_username = account + "." + user
@@ -33,3 +51,29 @@ def get_JWT():
     if isinstance(token, bytes):
         token = token.decode('utf-8')
     return token
+
+def get_user_by_email(email: str):
+    conn = get_snowflake_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            "SELECT USER, PASSWORD, ROLE FROM SNOWFLAKE_INTELLIGENCE.STREAMLITAI.TM_EMPLOYEE_CREDENTIALS WHERE USER=%s",
+            (email,)
+        )
+        return cur.fetchone()
+    finally:
+        cur.close()
+        conn.close()
+
+def set_user_password(email: str, password: str):
+    conn = get_snowflake_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            "UPDATE SNOWFLAKE_INTELLIGENCE.STREAMLITAI.TM_EMPLOYEE_CREDENTIALS SET PASSWORD=%s WHERE USER=%s",
+            (password, email)
+        )
+        conn.commit()
+    finally:
+        cur.close()
+        conn.close()
