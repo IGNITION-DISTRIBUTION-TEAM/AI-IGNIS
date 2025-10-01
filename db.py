ab5b0c2bf099f9e981e5849950addff27ac29f19
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-10-01 02:17:23 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,27 @@
 
+
+account = "pm58521".upper()
+user = "STREAMLITAI".upper()
+qualified_username = account + "." + user
+fingerprint_from_describe_user = "SHA256:3OngP5lVHGHb6tKCzjBNkIq7hOmAHNjzTxq4ZuHNll8="
+
+now = datetime.now(timezone.utc)
+lifetime = timedelta(minutes=59)
+
+# Construct JWT payload with issuer, subject, issue time, and expiration time
+payload = {
+    "iss": qualified_username + '.' + fingerprint_from_describe_user,
+    "sub": qualified_username,
+    "iat": now,
+    "exp": now + lifetime
+}
+
+encoding_algorithm = "RS256"
+
+private_key_path = "keys/rsa_key.p8"
+with open(private_key_path, "rb") as pem_in:
+    private_key = load_pem_private_key(pem_in.read(), password=None, backend=default_backend())
+    token = jwt.encode(payload, key=private_key, algorithm=encoding_algorithm)
+
+if isinstance(token, bytes):
+    token = token.decode('utf-8')
