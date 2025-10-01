0178a399650ba64fc175215abf5864a9ab2c12da
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-10-01 02:28:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,37 +29,6 @@ from db import get_JWT
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-HOST = "pm58521.east-us-2.azure.snowflakecomputing.com"
-DATABASE = "SNOWFLAKE_INTELLIGENCE"
-SCHEMA = "AGENTS"
-AGENT =  "IGNITION_POLICIES_ASSISTANT"
-
-account = "pm58521".upper()
-user = "STREAMLITAI".upper()
-qualified_username = account + "." + user
-fingerprint_from_describe_user = "SHA256:3OngP5lVHGHb6tKCzjBNkIq7hOmAHNjzTxq4ZuHNll8="
-
-now = datetime.now(timezone.utc)
-lifetime = timedelta(minutes=59)
-
-# Construct JWT payload with issuer, subject, issue time, and expiration time
-payload = {
-    "iss": qualified_username + '.' + fingerprint_from_describe_user,
-    "sub": qualified_username,
-    "iat": now,
-    "exp": now + lifetime
-}
-
-encoding_algorithm = "RS256"
-
-private_key_path = "keys/rsa_key.p8"
-with open(private_key_path, "rb") as pem_in:
-    private_key = load_pem_private_key(pem_in.read(), password=None, backend=default_backend())
-    token = jwt.encode(payload, key=private_key, algorithm=encoding_algorithm)
-
-if isinstance(token, bytes):
-    token = token.decode('utf-8')
-
 db_token = get_JWT()
 
 def agent_run() -> requests.Response:
