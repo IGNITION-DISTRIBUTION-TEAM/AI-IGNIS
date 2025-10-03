67516013854d512d91e93b5848eb8437672498d7
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-10-03 17:53:36 +0200

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -197,11 +197,11 @@ with col2:
         </h2>
         """, unsafe_allow_html=True)
         
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
-            
-            for message in st.session_state.messages:
-                render_message(message)
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
         
-            if user_input := st.chat_input("How can I help you?"):
-                process_new_message(prompt=user_input)
+        for message in st.session_state.messages:
+            render_message(message)
+    
+        if user_input := st.chat_input("How can I help you?"):
+            process_new_message(prompt=user_input)
