6deaf70f9b9c7e85f2f9366b91f60ea4255010d6
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2025-10-03 17:47:28 +0200

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -199,13 +199,13 @@ with col2:
 
         with st.container(horizontal_alignment="center", vertical_alignment="center"):
         
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-        
-        for message in st.session_state.messages:
-            render_message(message)
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+            
+            for message in st.session_state.messages:
+                render_message(message)
 
         with st.container(horizontal_alignment="bottom", vertical_alignment="center"):
         
-        if user_input := st.chat_input("How can I help you?"):
-            process_new_message(prompt=user_input)
+            if user_input := st.chat_input("How can I help you?"):
+                process_new_message(prompt=user_input)
